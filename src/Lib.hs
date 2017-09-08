b734cb79cfbc8bf433774b2c98a49604be0e4e3c
Marcelo Lazaroni <lazaronijunior@gmail.com>
2017-09-08 17:21:11 +0100

    Add compilation info and handle lack of headers

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,18 +1,23 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Lib (someFunc) where
 
+import qualified Data.Binary as Binary
 import qualified Data.ByteString.Lazy as BL
 import Data.ByteString.Lazy.Char8 (pack, unpack)
 import Data.Csv (HasHeader (NoHeader), decode, encode)
 import Data.Foldable (Foldable)
 import Data.Monoid (Monoid, mappend, mempty)
+import Data.Time (diffUTCTime, getCurrentTime)
 import Data.Vector (Vector, (!))
 import qualified Data.Vector as Vector
 import System.Environment (getArgs)
 
 someFunc :: IO ()
 someFunc =  do
+    startTime <- getCurrentTime
     args <- getArgs
+    putStrLn "Loading file..."
+
     if length args < 3 then
         putStrLn "You must provide three arguments.  resourceName fileSource and fileTarget. In this order"
     else
@@ -23,6 +28,7 @@ someFunc =  do
         in
             do
                 csvData <- BL.readFile fileSource
+                putStrLn "Parsing data..."
                 let
                     eData = decode NoHeader csvData :: Either String (Vector (Vector BL.ByteString))
                 case eData of
@@ -31,33 +37,31 @@ someFunc =  do
 
                     Right value ->
                         do
+                            putStrLn "Converting data..."
                             writeFile fileTarget . unpack . encode . processData resourceName $ value
+                            completionTime <- getCurrentTime
+
+                            let timeDiff = show . floor . toRational . diffUTCTime completionTime  $ startTime
+
+                            putStrLn $ "Time elapsed in seconds: " ++ timeDiff ++ "s"
                             putStrLn "Success"
 
 
-processData :: String -> Vector (Vector BL.ByteString) -> [(BL.ByteString, BL.ByteString, BL.ByteString, BL.ByteString)]
+processData :: String -> Vector (Vector BL.ByteString) -> [(BL.ByteString, String, BL.ByteString, BL.ByteString)]
 processData strResourceName raw =
-    let
-        headers = Vector.head raw
+    Vector.toList $ indexedFoldMap processRow raw
+    where
         resourceName = pack strResourceName
-        contentRows = Vector.tail raw
-    in
-        Vector.toList $
-        indexedFoldMap
-            (\rowIndex row ->
-                let
-                    date = Vector.head row
-                in
-                    Vector.imap
-                        (\colIndex cellContent ->
-                            let
-                                cellHeader = headers ! (colIndex + 1)
-                            in
-                                (date, cellHeader, cellContent, resourceName)
-                        )
-                        (Vector.tail row)
-            )
-            contentRows
+
+        processRow rowIndex row =
+            Vector.imap createCell $ Vector.tail row
+            where
+                date =
+                    Vector.head row
+
+                createCell colIndex cellContent =
+                    (date, "PATH_" ++ show rowIndex , cellContent, resourceName)
+
 
 indexedFoldMap :: Monoid m => (Int -> a -> m) -> Vector a -> m
 indexedFoldMap f v =
